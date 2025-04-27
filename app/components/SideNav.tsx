500e9d4bca414e2b46b919cc2eebfd80c1e227d2
Santoshsirji <santoshraut9862281653@gmail.com>
2025-04-27 16:29:36 -0400

    some more changes

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -1,17 +1,7 @@
 'use client';
 
-interface NavItem {
-    href: string;
-    label: string;
-    icon: React.ElementType;
-}
-
 export function SideNav() {
     
-    const navItems: NavItem[] = [
-        
-    ];
-
     return (
         <div className="hidden border-r bg-muted/40 md:block">
             <div className="flex h-full max-h-screen flex-col gap-2">
@@ -21,17 +11,9 @@ export function SideNav() {
                 </div>
                 <div className="flex-1">
                     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                        {/* Commenting out NavLink usage as component is not found */}
-                        {/* {navItems.map((item) => (
-                            <NavLink key={item.href} href={item.href} icon={item.icon}>
-                                {item.label}
-                            </NavLink>
-                        ))} */}
                     </nav>
                 </div>
                 <div className="mt-auto p-4">
-                    {/* Commenting out SignOutButton usage as component is not found */}
-                    {/* <SignOutButton /> */}
                 </div>
             </div>
         </div>
